dec051751e75bdd2f569889a2f28b4402a097f03
Mateus Boiani <mateuz.boiani@gmail.com>
2019-06-07 16:29:34 -0300

    almost ready

--- source/Aminoacids.cpp
+++ source/Aminoacids.cpp
@@ -42,30 +42,30 @@ size_t AminoAcid::getSize(){
 }
 
 void AminoAcid::showAminoList(){
-  std::cout << " |----------------|-----|-----|-----|" << std::endl;
-  std::cout << " |  Amino Acid    | 3LC | 1LC | AB  |" << std::endl;
-  std::cout << " |----------------|-----|-----|-----|" << std::endl;
-  std::cout << " |  Alanine       | Ala |  A  |  A  |" << std::endl;
-  std::cout << " |  Arginine      | Arg |  R  |  B  |" << std::endl;
-  std::cout << " |  Asparagine    | Asn |  N  |  B  |" << std::endl;
-  std::cout << " |  Aspartic acid | Asp |  D  |  B  |" << std::endl;
-  std::cout << " |  Cysteine      | Cys |  C  |  A  |" << std::endl;
-  std::cout << " |  Glutamic acid | Glu |  E  |  B  |" << std::endl;
-  std::cout << " |  Glutamine     | Gln |  Q  |  B  |" << std::endl;
-  std::cout << " |  Glycine       | Gly |  G  |  A  |" << std::endl;
-  std::cout << " |  Histidine     | His |  H  |  B  |" << std::endl;
-  std::cout << " |  Isoleucine    | Ile |  I  |  A  |" << std::endl;
-  std::cout << " |  Leucine       | Leu |  L  |  A  |" << std::endl;
-  std::cout << " |  Lysine        | Lys |  K  |  B  |" << std::endl;
-  std::cout << " |  Methionine    | Met |  M  |  A  |" << std::endl;
-  std::cout << " |  Phenylalanine | Phe |  F  |  B  |" << std::endl;
-  std::cout << " |  Proline       | Pro |  P  |  A  |" << std::endl;
-  std::cout << " |  Serine        | Ser |  S  |  B  |" << std::endl;
-  std::cout << " |  Threonine     | Thr |  T  |  B  |" << std::endl;
-  std::cout << " |  Tryptophan    | Trp |  W  |  B  |" << std::endl;
-  std::cout << " |  Tyrosine      | Tyr |  Y  |  B  |" << std::endl;
-  std::cout << " |  Valine        | Val |  V  |  A  |" << std::endl;
-  std::cout << " |----------------|-----|-----|-----|" << std::endl;
+  std::cout << " |----------------|-----|-----|-----| " << std::endl;
+  std::cout << " |  Amino Acid    | 3LC | 1LC | AB  | " << std::endl;
+  std::cout << " |----------------|-----|-----|-----| " << std::endl;
+  std::cout << " |  Alanine       | Ala |  A  |  A  | " << std::endl;
+  std::cout << " |  Arginine      | Arg |  R  |  B  | " << std::endl;
+  std::cout << " |  Asparagine    | Asn |  N  |  B  | " << std::endl;
+  std::cout << " |  Aspartic acid | Asp |  D  |  B  | " << std::endl;
+  std::cout << " |  Cysteine      | Cys |  C  |  A  | " << std::endl;
+  std::cout << " |  Glutamic acid | Glu |  E  |  B  | " << std::endl;
+  std::cout << " |  Glutamine     | Gln |  Q  |  B  | " << std::endl;
+  std::cout << " |  Glycine       | Gly |  G  |  A  | " << std::endl;
+  std::cout << " |  Histidine     | His |  H  |  B  | " << std::endl;
+  std::cout << " |  Isoleucine    | Ile |  I  |  A  | " << std::endl;
+  std::cout << " |  Leucine       | Leu |  L  |  A  | " << std::endl;
+  std::cout << " |  Lysine        | Lys |  K  |  B  | " << std::endl;
+  std::cout << " |  Methionine    | Met |  M  |  A  | " << std::endl;
+  std::cout << " |  Phenylalanine | Phe |  F  |  B  | " << std::endl;
+  std::cout << " |  Proline       | Pro |  P  |  A  | " << std::endl;
+  std::cout << " |  Serine        | Ser |  S  |  B  | " << std::endl;
+  std::cout << " |  Threonine     | Thr |  T  |  B  | " << std::endl;
+  std::cout << " |  Tryptophan    | Trp |  W  |  B  | " << std::endl;
+  std::cout << " |  Tyrosine      | Tyr |  Y  |  B  | " << std::endl;
+  std::cout << " |  Valine        | Val |  V  |  A  | " << std::endl;
+  std::cout << " |----------------|-----|-----|-----| " << std::endl;
 }
 
 std::string AminoAcid::convertSequence(std::string seq){
--- source/main.cpp
+++ source/main.cpp
@@ -6,27 +6,36 @@ int main(int argc, char * argv[]){
 
   if( argc == 1 ){
     printf("The amino acid sequence must be passed as parameters.\n");
+    exit(-1);
   }
 
+  if( argc == 2 ){
+    std::string T(argv[1]);
 
-  AminoAcid * a = new AminoAcid();
+    AminoAcid * a = new AminoAcid();
 
-  std::cout << a->getSize() << std::endl;
+    T = a->convertSequence(T);
+    std::cout << T << std::endl;
+  }
 
-  a->showAminoList();
-
-  //std::cout << a->getAmino('X') << std::endl;
-
-
-  std::string T = "GVPINVSCTGSPQCIKPCKDAGMRFGKCMNRKCHCTPK";
-
-  T = a->convertSequence(T);
-
-  std::cout << T << std::endl;
-
-  std::string res = "AAAABABABABABAABAABBAAABBABAABBBABABAB";
-
-  if( T != res ){ printf("TEST FAILED\n"); } else { printf("TEST PASSED\n"); }
+  // AminoAcid * a = new AminoAcid();
+  //
+  // std::cout << a->getSize() << std::endl;
+  //
+  // a->showAminoList();
+  //
+  // //std::cout << a->getAmino('X') << std::endl;
+  //
+  //
+  // std::string T = "GVPINVSCTGSPQCIKPCKDAGMRFGKCMNRKCHCTPK";
+  //
+  // T = a->convertSequence(T);
+  //
+  // std::cout << T << std::endl;
+  //
+  // std::string res = "AAAABABABABABAABAABBAAABBABAABBBABABAB";
+  //
+  // if( T != res ){ printf("TEST FAILED\n"); } else { printf("TEST PASSED\n"); }
 
   return 0;
 }
